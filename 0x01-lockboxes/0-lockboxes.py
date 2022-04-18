acf0ea6081bdeefcb50a7ee934c28ee4b06065d0
tihitna <tihitnasete@gmail.com>
2022-04-18 08:32:55 +0000

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,28 +1,36 @@
 #!/usr/bin/python3
-"""Determines if all boxes can be opened."""
+"""
+Module 0-lockboxes
+"""
 
 
 def canUnlockAll(boxes):
-        """method that determines if all the boxes can be opened."""
-            n = len(boxes)
+            """
+    You have n number of locked boxes in front of you.
+    Each box is numbered sequentially from 0 to n - 1,
+    and each box may contain keys to the other boxes.
+    Determine if all the boxes can be opened.
+    """
+                if boxes is None:
+                                return False
+                            if len(boxes) == 1:
+                                            return True
+                                        # Track the visited boxes
+                                            visited = set()
+                                                # The first box is unlocked
+                                                    visited.add(0)
 
-                if n == 0:
-                            return True
+                                                        # Use a stack to do a Depth First Search
+                                                            stack = []
+                                                                stack.append(0)
 
-                            opened = []
-
-                                for i in range(0, n):
-                                            opened.append(False)
-
-                                                opened[0] = True
-                                                    openBox(boxes, opened, 0)
-
-                                                        return all(opened)
-
-
-                                                    def openBox(boxes, opened, nBox):
-                                                            """Open boxes recursively."""
-                                                                for key in boxes[nBox]:
-                                                                            if key < len(boxes) and opened[key] is False:
-                                                                                            opened[key] = True
-                                                                                                        openBox(boxes, opened, key)
+                                                                    while stack:
+                                                                                    keys = boxes[stack.pop()]
+                                                                                            for key in keys:
+                                                                                                                if key not in visited:
+                                                                                                                                        visited.add(key)
+                                                                                                                                                        stack.append(key)
+                                                                                                                                                            # All have been visited if the
+                                                                                                                                                                # number of boxes is equal to the
+                                                                                                                                                                    # number of visited boxes
+                                                                                                                                                                        return len(boxes) == len(visited)
